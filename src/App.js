7681500d8cf91a222228f5e3a27a38ebca349489
Shipix <shipix@hotmail.fr>
2021-01-14 16:27:26 +0100

    Création du component profil et importation dans app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Profil from "./Profil.js"
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Profil />
+        <Profil />
+        <Profil />
+
+      </div>
+    )
+  }
+}
---
+++ src/Profil.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Profil extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Prénom : Nicolas</h1>
+                <h2>Nom : Primo</h2>
+                <p>Description : Un jeune et beau coach puissant(aimez vous guys)</p>
+            </div>
+        )
+    }
+}
